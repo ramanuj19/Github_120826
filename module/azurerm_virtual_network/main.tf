8e790c93ef311dc92412ff469c06bae393d9738c
ramanuj19 <ramanujsingh84@gmail.com>
2026-08-12 17:01:55 +0530

    added yml

--- module/azurerm_virtual_network/main.tf
+++ module/azurerm_virtual_network/main.tf
@@ -4,7 +4,7 @@ variable "vnet" {
 resource "azurerm_virtual_network" "virtualnetwork" {
   for_each = var.vnet
   resource_group_name = each.value.rg_name
-  location = each.value.location
+  location = each.value.rg_location
   name = each.value.vnet_name
   address_space = each.value.address_space
 }
